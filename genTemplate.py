5a306c8051b99377da011ec41b9f3c55d6ce61f3
Greg Rell <gregory.rell@universalorlando.com>
2019-10-22 16:24:09 -0400

    More work with templates

--- genTemplate.py
+++ genTemplate.py
@@ -1,6 +1,6 @@
 import xlsxwriter as xls
-workbook=xls.Workbook('Template.xlsx')
-worksheet=workbook.add_worksheet('FMEA')
+#workbook=xls.Workbook('Template.xlsx')
+#worksheet=workbook.add_worksheet('FMEA')
 
 expenses =(
     ['rent',100],
@@ -15,9 +15,39 @@ for item, cost in expenses:
     row+=1
 
 
-workbook.close()
+#workbook.close()
 
 import pyexcel as pe
-sheet=pe.get_sheet(file_name="Template.xlsx")
+book=pe.get_book(file_name="Template.xlsx")
+print(book.sheet_names())
+sheet=book.sheet_by_name(book.sheet_names()[1])
 print(sheet)
+
+#print(sheet[1,1])
+
+
+
+fp = open('styles.txt')
+styles=fp.readlines()
+styles=[x.strip() for x in styles]
+#styles=[x.replace(' ','') for x in styles]
+
+from docx import Document
+
+document = Document()
+table = document.add_table(rows=3,cols=3)
+table.style = 'Colorful Grid Accent 1'
+cell=table.cell(0,1)
+cell.text=sheet[1,1]
+print(table.style)
+
+for style in styles:
+    table = document.add_table(rows=3, cols=3)
+    table.style = style
+    cell = table.cell(0, 1)
+    cell.text = style
+    print(table.style)
+
+
+document.save('demo.docx')
 
